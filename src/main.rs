7938876ed9a7b73a428ebea9b31f0cd73a3498b0
jolade okunlade <jokunlade@gmail.com>
2025-06-24 23:30:36 +0100

    Async function created

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use trpl::Html;
 
 async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
+    let response_text = trpl::get(url).await.text().await;
     let response_text = response.text().await;
     Html::parse(&response_text)
         .select_first("title")
         .map(|title_element| title_element.inner_html())
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    })
 }
